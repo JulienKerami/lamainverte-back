00e312be1c82275c3c1e6852b64891b3547653d0
dorotheetartarin <dorothee.tartarin@hotmail.fr>
2024-01-12 15:42:29 +0100

    fix(create_tables) : resolution bug files

--- src/sequelize-client.js
+++ src/sequelize-client.js
@@ -7,14 +7,15 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   define: {
-    //createdAt: "created_at", // Pour TOUS nos modèles, on leur met le champs `createdAt` mappé (lié) vers le champs `created_at` en BDD
-    //updatedAt: "updated_at" // Pour TOUS nos modèles, on leur met le champs `updatedAt` mappé (lié) vers le champs `updated_at` en BDD
+    createdAt: "created_at", // Pour TOUS nos modèles, on leur met le champs `createdAt` mappé (lié) vers le champs `created_at` en BDD
+    updatedAt: "updated_at" // Pour TOUS nos modèles, on leur met le champs `updatedAt` mappé (lié) vers le champs `updated_at` en BDD
   }
 });
 
 module.exports = sequelize;
 
 // Pour tester temporairement --> Test réussi le 12/01/2024
+
 // sequelize.authenticate()
 //   .then(() => { console.log("success !"); })
 //   .catch(error => console.error(error));
