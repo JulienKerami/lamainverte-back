2779da85a65caa5cca2fbe8c660d14ec7a32592e
dorotheetartarin <dorothee.tartarin@hotmail.fr>
2024-01-15 17:14:51 +0100

    feat(zone) : createZone + getAllZones

--- src/controllers/zone.controller.js
+++ src/controllers/zone.controller.js
@@ -2,15 +2,40 @@
 const { Zone } = require('../models/index.model');
 
 async function getAllZones(req, res) {
+
+  const userId = 2; // TODO: pour faire des essais, à voir comment on récupère l'id de l'utilisateur connecté
+
   try {
-    const zones = await Zone.findAll();
-    res.json(zones);
+    const zones = await Zone.findAll({ where: { user_id: userId } });
+    res.status(200).json(zones);
+
   } catch (error) {
     res.status(500).json(error);
   }
 }
 
 
+async function createZone(req, res) {
+
+  try {
+    const {name, userId} = req.body;
+
+    const zone = await Zone.create({
+      name: name,
+      user_id: userId
+    
+    });
+
+    res.status(200).json(zone);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+
+
 module.exports = {
-  getAllZones
+  getAllZones,
+  createZone
 };
--- src/routers/zone.routeur.js
+++ src/routers/zone.routeur.js
@@ -3,6 +3,7 @@ const zoneController = require('../controllers/zone.controller');
 const zoneRouter = Router();
 
 zoneRouter.get("/zone", zoneController.getAllZones);
+zoneRouter.post("/zone", zoneController.createZone);
 
 
 module.exports = zoneRouter;
