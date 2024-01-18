304a45c4715f2aa11deabf583b6b7079074f99b1
Julien Kerami <julien.kerami@gmail.com>
2024-01-18 14:01:45 +0100

    feat(family) : create family controller and getAllFamilies

---
+++ src/controllers/family.controller.js
@@ -0,0 +1,12 @@
+const { Family } = require('../models/index.model');
+
+async function getAllFamilies(req, res) {
+
+    try {
+        const families = await Family.findAll();
+        res.status(200).json(families);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+module.exports = { getAllFamilies};
