1ec81ab2c6cc83d751fdeed096bed168409d0044
dorotheetartarin <dorothee.tartarin@hotmail.fr>
2024-01-18 19:02:14 +0100

    fix(zone) : add on cascade create_tables

--- src/controllers/zone.controller.js
+++ src/controllers/zone.controller.js
@@ -1,38 +1,52 @@
-const { Zone } = require('../models/index.model');
-const { Vegetable } = require('../models/index.model');
+const { Zone} = require('../models/index.model');
+
+const jwt = require('jsonwebtoken');
+
+function getUserIdFromToken(authorizationHeader) {
+  const token = authorizationHeader.split(' ')[1];
+  return jwt.decode(token).id;
+}
 
 async function createZone(req, res) {
-
   try {
-    const {name, userId} = req.body; // TODO : Vérifier comment arrive le userId
-    console.log(req.body);
+    const userId = getUserIdFromToken(req.headers.authorization);
+
+    const { name } = req.body;
+
+    // Do not allow an empty "name" field
+    if (!name) {
+      res.status(400).json({ message: "Missing name" });
+      return;
+    }
+
+    // Check if the name is already used
+    const nameAlreadyUsed = await Zone.findOne({
+      where: {
+        user_id: userId,
+        name: name,
+      },
+    });
+
+    if (nameAlreadyUsed) {
+      res.status(400).json({ message: "Name already used" });
+      return;
+    }
 
     const zone = await Zone.create({
       user_id: userId,
-      name: name     
-    
+      name: name,
     });
 
     res.status(200).json(zone);
-
   } catch (error) {
     res.status(500).json(error);
   }
 }
 
-
 async function deleteZone(req, res) {
   try {
     const zoneId = parseInt(req.params.zoneId);
-
-    // Recherche et suppression des Vegetables associés en premier
-    await Vegetable.destroy({
-      where: {
-        zone_id: zoneId,
-      },
-    });
-
-    // Vous pouvez maintenant supprimer en toute sécurité la Zone
+  
     await Zone.destroy({
       where: {
         id: zoneId,
@@ -40,34 +54,50 @@ async function deleteZone(req, res) {
     });
 
     res.status(204).end();
+
   } catch (error) {
     res.status(500).json(error);
   }
 }
 
-
 async function updateZone(req, res) {
   try {
-    const zoneId = parseInt(req.params.zoneId);
+    const userId = getUserIdFromToken(req.headers.authorization);
 
-    const {name}=req.body;
+    const zoneId = parseInt(req.params.zoneId);
+    const { name } = req.body;
+
+    // Do not allow an empty "name" field
+    if (!name) {
+      res.status(400).json({ message: "Missing name" });
+      return;
+    }
+
+    // Check if the name is already used
+    const nameAlreadyUsed = await Zone.findOne({
+      where: {
+        user_id: userId,
+        name: name,
+      },
+    });
+
+    if (nameAlreadyUsed) {
+      res.status(400).json({ message: "Name already used" });
+      return;
+    }
 
     const zone = await Zone.findByPk(zoneId);
 
-    await zone.update({name : name});
+    await zone.update({ name: name });
 
     res.status(200).json(zone);
   } catch (error) {
     res.status(500).json(error);
   }
-
 }
-
-
 
 module.exports = {
   createZone,
   deleteZone,
-  updateZone
-
+  updateZone,
 };
