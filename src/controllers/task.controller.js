f5e67905ff5a494569602f9c7be06c5ec0aff009
Julien Kerami <julien.kerami@gmail.com>
2024-01-22 15:44:36 +0100

    fix(tasks) : create the getalltasks controller for tasks to do and update doc

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,26 +1,48 @@
-const { Task } = require('../models/index.model');
+const { Vegetable, Zone, User, Task } = require("../models/index.model");
 
-async function getAllTasks(req, res) { 
-  
+const { getUserIdFromToken } = require("./utils");
+
+async function getAllTasks(req, res) {
+  const userId = getUserIdFromToken(req.headers.authorization);
+
+  // récupérer toutes les tâches "à faire" d'un utilisateur
+
   try {
-    const tasks = await Task.findAll();
-    
+    const tasks = await Task.findAll({
+      include: [
+        {
+          model: Vegetable,
+          include: [
+            {
+              model: Zone,
+              include: [
+                {
+                  model: User,
+                  where: { id: userId },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      where: {
+        status_code: 1,
+      },
+    });
+
     res.status(200).json(tasks);
-  
   } catch (error) {
     res.status(500).json(error);
   }
 }
 
 async function getOneTask(req, res) {
-
   const taskId = parseInt(req.params.taskId);
-  
+
   try {
     const task = await Task.findByPk(taskId);
-    
+
     res.status(200).json(task);
-  
   } catch (error) {
     res.status(500).json(error);
   }
@@ -28,7 +50,7 @@ async function getOneTask(req, res) {
 
 // Update task
 
-// Quand on click sur un tache et que l'utilisateur coche "fait" 
+// Quand on click sur un tache et que l'utilisateur coche "fait"
 // -> Mettre la date effective
 // -> Mettre le statut : "fait"
 // -> Mettre le statut code correspondant,
@@ -37,8 +59,7 @@ async function getOneTask(req, res) {
 // -> Si le type est planting, recherche la tache harvesting ayant le même vegetable id et on lui change son statut en "à faire" (+ statut code correspondant)
 // -> Si le type est harvesting, toutes les tâches sont ok
 
-
 module.exports = {
   getAllTasks,
-  getOneTask
+  getOneTask,
 };
--- src/routers/task.routeur.js
+++ src/routers/task.routeur.js
@@ -1,17 +1,50 @@
 const { Router } = require("express");
-const taskController = require('../controllers/task.controller');
+const taskController = require("../controllers/task.controller");
 const taskRouter = Router();
 
 /**
  * @swagger
  * /tasks:
  *   get:
- *     summary: Récupérer la liste de toutes les tâches
+ *     summary: Récupérer la liste de toutes les tâches avec un status_code de 1 pour l'utilisateur authentifié
+ *     description: Renvoie toutes les tâches ayant un status_code de 1 et liées à des légumes, qui sont eux-mêmes liés à des zones associées à l'utilisateur authentifié.
  *     responses:
  *       200:
- *         description: Succès renvoie la liste de toutes les tâches
+ *         description: Succès - renvoie la liste de toutes les tâches correspondantes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                   type:
+ *                     type: string
+ *                   status:
+ *                     type: string
+ *                   status_code:
+ *                     type: number
+ *                   start_date_period:
+ *                     type: string
+ *                     format: date-time
+ *                   end_date_period:
+ *                     type: string
+ *                     format: date-time
+ *                   effective_date:
+ *                     type: string
+ *                     format: date-time
+ *                   comment:
+ *                     type: string
+ *                   vegetable_id:
+ *                     type: number
+ *                   Vegetable:
+ *                     type: object
+ *                     $ref: '#/components/schemas/Vegetable'
  *       500:
  *         description: Erreur serveur
+ *
  * /tasks/{taskId}:
  *   get:
  *     summary: Récupérer une tâche par son id
@@ -20,18 +53,20 @@ const taskRouter = Router();
  *         in: path
  *         required: true
  *         schema:
- *           type: string
+ *           type: integer
  *     responses:
  *       200:
- *         description: Succès renvoie la tâche demandée
+ *         description: Succès - renvoie la tâche demandée
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
  *       500:
  *         description: Erreur serveur
  */
 
-
 taskRouter.get("/tasks", taskController.getAllTasks);
 taskRouter.get("/tasks/:taskId", taskController.getOneTask);
 //taskRouter.patch("/tasks/:taskId", taskController.updateTask);
-
 
 module.exports = taskRouter;
